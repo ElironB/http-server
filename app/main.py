772e111e61427a3cc9e50c732fbfa4997b243cc5
ElironB <Elironb11@gmail.com>
2024-05-24 11:18:36 +0300

    3rd Phase

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
 # Uncomment this to pass the first stage
 import socket as s
-import http.server
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
+    
     # Uncomment this to pass the first stage
     #
     server_socket = s.create_server(("localhost", 4221))
     server_socket.listen()
-
-    while True:
-        connection , address = server_socket.accept()
-        
-        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n") 
-
-        connection.close()
+    connection , address = server_socket.accept()
+    data = connection.recv(1024).decode("utf-8")
+    path = data.split(" ")[1]
+    if path == "/":
+        connection.send(b"HTTP/1.1 200 OK\r\n\r\n") 
+    else:
+        connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n") 
 
 
 if __name__ == "__main__":
