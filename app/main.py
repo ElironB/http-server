e9ac8cfb30889e2089af38b9ee02d975d458c283
ElironB <Elironb11@gmail.com>
2024-05-24 17:41:38 +0300

    6th phase

--- app/main.py
+++ app/main.py
@@ -5,43 +5,43 @@ import socket as s
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    
-    # Uncomment this to pass the first stage
-    #
     server_socket = s.create_server(("localhost", 4221))
-    server_socket.listen()
-    connection , address = server_socket.accept()
-    data = connection.recv(4096).decode("utf-8")
-    path = data.split(" ")[1]
-    if path.startswith("/echo"):
-        st = path.split("/")[-1].encode("utf-8")
-        length = str(len(st)).encode("utf-8")
-        response = (
-            b"HTTP/1.1 200 OK\r\n"
-            b"Content-Type: text/plain\r\n"
-            b"Content-Length: " + length + b"\r\n"
-            b"\r\n" + st
-        )
-        connection.send(response)
-    elif path == "/user-agent":
-        headers = data.split("\r\n")
-        agent = ""
-        for header in headers:
-            if header.startswith("User-Agent:"):
-                agent = header.split(": ", 1)[1].encode("utf-8")
-                break
-        length = str(len(agent)).encode("utf-8")
-        response = (
-            b"HTTP/1.1 200 OK\r\n"
-            b"Content-Type: text/plain\r\n"
-            b"Content-Length: " + length + b"\r\n"
-            b"\r\n" + agent
-        )     
-        connection.send(response)
-    elif path == "/":
-        connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    else:
-        connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")         
+    server_socket.listen()  
+    # Uncomment this to pass the first stage
+    while True: 
+        connection , address = server_socket.accept()
+        print(f"Connected by {address}")
+        data = connection.recv(4096).decode("utf-8")
+        path = data.split(" ")[1]
+        if path.startswith("/echo"):
+            st = path.split("/")[-1].encode("utf-8")
+            length = str(len(st)).encode("utf-8")
+            response = (
+                b"HTTP/1.1 200 OK\r\n"
+                b"Content-Type: text/plain\r\n"
+                b"Content-Length: " + length + b"\r\n"
+                b"\r\n" + st
+            )
+            connection.send(response)
+        elif path == "/user-agent":
+            headers = data.split("\r\n")
+            agent = ""
+            for header in headers:
+                if header.startswith("User-Agent:"):
+                    agent = header.split(": ", 1)[1].encode("utf-8")
+                    break
+            length = str(len(agent)).encode("utf-8")
+            response = (
+                b"HTTP/1.1 200 OK\r\n"
+                b"Content-Type: text/plain\r\n"
+                b"Content-Length: " + length + b"\r\n"
+                b"\r\n" + agent
+            )     
+            connection.send(response)
+        elif path == "/":
+            connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        else:
+            connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")         
 
 
 if __name__ == "__main__":
