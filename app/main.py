ca7a8a8344dd81b6ff1c2b34ebced770ccbd41a8
ElironB <Elironb11@gmail.com>
2024-05-24 11:56:14 +0300

    4th Phase

--- app/main.py
+++ app/main.py
@@ -11,10 +11,18 @@ def main():
     server_socket = s.create_server(("localhost", 4221))
     server_socket.listen()
     connection , address = server_socket.accept()
-    data = connection.recv(1024).decode("utf-8")
+    data = connection.recv(4096).decode("utf-8")
     path = data.split(" ")[1]
-    if path == "/":
-        connection.send(b"HTTP/1.1 200 OK\r\n\r\n") 
+    if path.startswith("/echo"):
+        st = path.split("/")[-1].encode("utf-8")
+        length = str(len(st)).encode("utf-8")
+        response = (
+            b"HTTP/1.1 200 OK\r\n"
+            b"Content-Type: text/plain\r\n"
+            b"Content-Length: " + length + b"\r\n"
+            b"\r\n" + st
+        )
+        connection.send(response)
     else:
         connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n") 
 
