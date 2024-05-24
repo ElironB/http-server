6dc1381a39cc51e51306a4fd3f90b4e377b2ef92
ElironB <Elironb11@gmail.com>
2024-05-24 11:59:59 +0300

    4th Phase

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def main():
     elif path == "/":
         connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
     else:
-        connection.send(b"HTTP/1.1 200 OK\r\n\r\n")         
+        connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")         
 
 
 if __name__ == "__main__":
