6f23141d57137e4004a8425d1f2bde00e3089b74
ElironB <Elironb11@gmail.com>
2024-05-24 09:20:33 +0300

    2nd Stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # Uncomment this to pass the first stage
-import socket
+import socket as s
 
 
 def main():
@@ -8,8 +8,9 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = s.create_server(("localhost", 4221))
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK \r\n\r\n") # wait for client
+    
 
 
 if __name__ == "__main__":
